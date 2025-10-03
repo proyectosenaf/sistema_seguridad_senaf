a793eebc2eb644aa892c9889be451541a5213723
Milton Casco <tunombreejemplo.com>
2025-10-03 16:29:30 -0600

    mensaje claro del cambio

--- server/src/models/Turno.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const TurnoSchema = new mongoose.Schema({
-  nombre: { type:String, required:true },        // "Día", "Noche", "12x12", etc.
-  horaInicio: String,                             // "06:00"
-  horaFin: String,                                // "18:00"
-}, { timestamps:true });
-export default mongoose.model("Turno", TurnoSchema);
--- server/src/models/Vehiculo.js
+++
@@ -1,8 +0,0 @@
-// models/Vehiculo.js
-import mongoose from "mongoose";
-const VehiculoSchema = new mongoose.Schema({
-  placa: { type:String, index:true },
-  marca: String, modelo: String, color: String, tipo: String
-}, { timestamps:true });
-export default mongoose.model("Vehiculo", VehiculoSchema);
-
