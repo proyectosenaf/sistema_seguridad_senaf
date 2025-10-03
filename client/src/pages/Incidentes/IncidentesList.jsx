5697d7348088cfd429d2bc3b486ff9abbcb8a6f0
Milton Casco <tunombreejemplo.com>
2025-10-03 17:59:42 -0600

    mensaje claro del cambio

---
+++ client/src/pages/Incidentes/IncidentesList.jsx
@@ -0,0 +1,5 @@
+// client/src/pages/Incidentes/IncidentesList.jsx
+import React from "react";
+export default function IncidentesList() {
+  return <div className="card">Módulo Gestión de Incidentes — próximamente</div>;
+}
