376ea4c75ec2779ae1404b579612ec2dd919319a
SenafSeg <proyectosenaf@gmail.com>
2026-02-09 23:08:18 -0600

    mensaje claro del cambio

--- client/infra/pm2.config.cjs
+++ client/infra/pm2.config.cjs
@@ -1,10 +1,25 @@
 module.exports = {
-apps: [
-{
-name: 'senaf-api',
-script: 'src/server.js',
-cwd: './api',
-env: { NODE_ENV: 'production' },
-},
-],
+  apps: [
+    {
+      name: "senaf",
+      script: "src/server.js",
+      cwd: "./api",
+
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 4000,
+
+        // 🔹 MongoDB Atlas (producción)
+        MONGODB_URI:"mongodb+srv://proyectosenaf_db_user:SENAFdb2025@senafcluster.vwwt8sy.mongodb.net/senafseg?retryWrites=true&w=majority" ,
+
+
+        // 🔹 Auth0 (si lo usas)
+        AUTH0_DOMAIN: "dev-0046gqmh011jo75x.us.auth0.com",
+        AUTH0_AUDIENCE: "https://senaf",
+
+        // 🔹 Opcional
+        IAM_DEV_ALLOW_ALL: "0"
+      }
+    }
+  ]
 };
